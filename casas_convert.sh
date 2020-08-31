6ca4fa93fe4d163a21b3f3d7c189bc3429b6c574
ASuherman <AnggaSuherman@users.noreply.github.com>
2020-08-31 15:58:58 +0900

    Create casas_convert.sh

---
+++ casas_convert.sh
@@ -0,0 +1,2 @@
+#!/bin/bash gawk 
+'{ printf "{¥"datetime¥" : ¥"%s %s¥", ¥"sensor¥" : ¥"%s¥", ¥"translate01¥": ¥"%s¥", ¥"translate02¥": ¥"%s¥", ¥"message¥":¥"%s¥", ¥"sensoractivity¥": ¥"%s¥", ¥"location¥": ¥"'${1}'¥" }¥n", $1, $2, $3, $4, $5, $6, $7 }' ${1}/${1}.rawdata.txt
