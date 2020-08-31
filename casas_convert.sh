55c2abe88a7d155322727add4bb21515cd555022
ASuherman <AnggaSuherman@users.noreply.github.com>
2020-08-31 17:36:26 +0900

    Update casas_convert.sh

--- casas_convert.sh
+++ casas_convert.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 gawk  '{ printf "{\"datetime\" : \"%s %s\",  \"sensor\" : \"%s\", \"translate01\": \"%s\",  \"translate02\": \"%s\", \"message\":\"%s\", \"sensoractivity\": \"%s\", \"location\": \"'${1}'\"   }\n", $1, $2, $3, $4, $5, $6, $7 }'  ${1}/${1}.rawdata.txt 
+
+for loc in `ls`; do
+   ./casas_convert.sh | /kafka_2.13-2.6.0/bin/kafka-console-producer.sh  --bootstrap-server localhost:9092 --topic casas
+done
